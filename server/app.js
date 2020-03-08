6723a34acb04b4528478ea9ffa040d96dcbb10d0
Sean Harrell <seanvharrell@gmail.com>
2020-03-08 01:35:49 +0000

    server is working

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const http = require ('http');
 
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+    res.status(200).send('server is working')
+});
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const server = require('./app');
 
+const PORT = process.env.PORT || 3000;
+
 server.listen(3000, function() {
-  console.log('Server is listening on http://localhost:3000');
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
