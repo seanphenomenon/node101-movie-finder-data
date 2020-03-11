a652ddf06c31152381103554e53ad275835fe94e
Sean Harrell <seanvharrell@gmail.com>
2020-03-11 23:35:06 +0000

    cache history and api call working

--- server/app.js
+++ server/app.js
@@ -1,15 +1,61 @@
 const express = require('express');
 const morgan = require('morgan');
 const http = require ('http');
+const axios = require('axios');
+const instance = axios.create();
+const cache = {}
 
-
+//App
 const app = express();
 
+//Morgan
 app.use(morgan('dev'));
 
+//First Route
 app.get('/', function(req, res){
-    res.status(200).send('server is working')
-});
+
+// movieId query parameter below returns empty object
+const movieId = req.query;
+console.log(movieId)
+
+// Object keys method below will target key value (ex. i,t,s) of object in index 0. 
+const keys = Object.keys(movieId)[0];
+
+// Object values method will target value (movie Id number or name of movie)
+const values = Object.values(movieId)[0];
+
+// target query values in URL 
+const url = (`http://www.omdbapi.com/?${keys}=${values}+&apikey=3119abb3`);
+
+
+// axios instance used to make get request to URL and verifying previous cache url requests
+if(cache[url]){
+    res.send(cache[url])
+} else{
+
+instance.get(url)
+    .then(response => cache[url] = response.data)
+    .then(response => res.send(response))
+    .catch(err => console.log(err))
+
+
+}});
+
+
+
+
+// response below creates object on webpage
+// res.json(movieId); 
+
+
+// var idKey below targets id of json object
+//  var idKey = req.query.i;
+
+
+
+// console.log("server is working")
+// res.status(200).send('server is working');
+
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const server = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+// Starting Server
 server.listen(3000, function() {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
