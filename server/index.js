0c90e3186b620f187e36f15e27e79757ffc88179
Sean Harrell <seanvharrell@gmail.com>
2020-03-24 05:49:57 +0000

    server edit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const server = require('./app');
 const PORT = process.env.PORT || 3000;
 
 // Starting Server
-server.listen(3000, function() {
+server.listen(PORT, function() {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
